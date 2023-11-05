342c797d86b48c10a3ee22d47b70da84dd34e65d
Honma Ryota <kabuprivate@gmail.com>
2023-11-05 22:55:33 +0900

    cssの適用方法（import・CSSモジュール・clsxライブラリ）

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body>{children}</body>
-      <p className="text-blue-500">aaa</p>
     </html>
   );
 }
