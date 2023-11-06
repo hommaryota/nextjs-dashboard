ec6bbc1c773169e7a3ab15f293c6d75fdfd843bf
Honma Ryota <kabuprivate@gmail.com>
2023-11-07 02:00:04 +0900

    レイアウトとページの作成

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({placeholder}: {placeholder: string}) {
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
